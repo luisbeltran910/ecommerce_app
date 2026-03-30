58c1f971dab03a51634ad767098e5b8c91f7aea0
luisbeltran910 <luisbeltran991@gmail.com>
2026-03-29 20:46:26 -0700

    feat: Added the apiUrl constant to connect to the backend

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  AppConstants._();
+
+  static const String apiUrl = 'http://localhost:8080/api/v1';
+  static const int connectTimeout = 30000;
+  static const int receiveTimeout= 30000;
+}
